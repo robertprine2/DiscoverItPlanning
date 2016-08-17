00c3f2fa66e14c57d1a8799ab92d92fd3acc6fe8
Robert Prine <robertprine2@yahoo.com>
2016-08-17 14:47:00 -0400

    Added information needed for first week

---
+++ Schema.js
@@ -0,0 +1,20 @@
+users = {
+	avatar = /*string link to cloudinary*/,
+	userName = /*string name chosen by user*/,
+	email = /*user email*/,
+	discoveries = /*integer of how many discoveries user has made*/,
+	edits = /*integer of how many objects user has editted*/,
+	confirms = /*integer of how many objects user has confirmed*/
+}
+
+
+
+discoveries = {
+	image = /*string link to cloudinary*/,
+	name = /*string name of object*/,
+	objectType = /*string Animal, Plant, Art, Vehicle, Food, or Pokemon*/,
+	discription = /*string discription of object*/,
+	location = /*string location where object was found*/,
+	discoveredBy = /*string user that discovered object*/,
+	dateDiscovered = /*date/time discovery was made*/
+}
